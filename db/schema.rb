404bfe89f34c01ba6de102eb5bee4bf197a2424d
Corrie <corrieleech@gmail.com>
2022-02-28 21:50:22 -0600

    adds dummy data to seed file for users, clubs, memberships and messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_28_223016) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_01_033456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "clubs", force: :cascade do |t|
     t.string "name"
     t.integer "work_id"
-    t.integer "isbn"
+    t.string "isbn"
     t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_28_223016) do
     t.integer "club_id"
     t.integer "user_id"
     t.string "body"
-    t.string "attribute"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# #Users
+
+# User.create(name: "Karen Johnson", email: "karen@aol.com", password: "password", password_confirmation: "password", location: "Columbus, Ohio", twitter: "@karenestkaren", about: "46 years young. Soccer mom of three. Live, laugh, love... wine!", image: "https://static.wikia.nocookie.net/muc/images/4/4f/Karen.jpg/revision/latest/scale-to-width-down/1200?cb=20200810021525")
+# User.create(name: "Sara Stewart", email: "sara@gmail.com", password: "password", password_confirmation: "password", location: "Milwaukee", twitter: "@cartwheels", about: "Fitness freak: I burn calories by jumping to conclusions.", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNyk_UDys8SolWqrebQcQwcETU_3OrlW2Vgw&usqp=CAU")
+# User.create(name: "Abby Keating", email: "abby@gmail.com", password: "password", password_confirmation: "password", location: "Waukesha, WI", twitter: "@abbyreads", about: "I don’t join social media platforms often. But, when I do, it’s worth a read.", image: "https://pbs.twimg.com/profile_images/1455996520050872328/c86jxMqc_400x400.jpg")
+# User.create(name: "Heather Aplin", email: "heather@gmail.com", password: "password", password_confirmation: "password", location: "Madison, WI", twitter: "@readwise", about: "A woman of mystery and power, whose power is exceeded only by her mystery", image: "https://m.media-amazon.com/images/M/MV5BMTQwMDY3MDc0OF5BMl5BanBnXkFtZTYwMzM3NTg3._V1_UY1200_CR86,0,630,1200_AL_.jpg")
+# User.create(name: "Gayle Sheer", email: "gayle@gmail.com", password: "password", password_confirmation: "password", location: "Chicago", twitter: "@gaylethesnail", about: "Coffee. Quilting. Mom of @Boggie the Doggie. And books.", image: "https://i.pinimg.com/280x280_RS/e2/5f/e1/e25fe10af7d1e442ed4ab30a32071cce.jpg")
+# User.create(name: "Christina Susan", email: "christina@gmail.com", password: "password", password_confirmation: "password", location: "Chicago", twitter: "@stinasusan", about: "When I'm not lifting heavy things, I'm reading.", image: "https://scontent-ort2-1.xx.fbcdn.net/v/t1.6435-9/69742187_10103546932420815_3735531751108247552_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=uHthYfiv204AX81fGT8&tn=2RJkG2OtKrbB3WsI&_nc_ht=scontent-ort2-1.xx&oh=00_AT_zdznrV8zydPgEtpulPlnWwcjMEIx0xcK9MZir3EwPpA&oe=62412BB1")
+# User.create(name: "Lilaaarrrgggghhhh", email: "lila@gmail.com", password: "password", password_confirmation: "password", location: "Chicago", twitter: "@lilargh", about: "Love reading but only on here because Corrie made me", image: "https://ocep.northwestern.edu/files/img/meet-the-team/lila-pic.jpg")
+# User.create(name: "Lost in a Book", email: "lost@gmail.com", password: "password", password_confirmation: "password", location: "Madison, WI", twitter: "@lostinabook", about: "Some say they get lost in books, but I find myself, again and again, in the pages of a good book. Humanly speaking, there is no greater teacher, no greater therapist, no greater healer of the soul, than a well-stocked library.", image: "https://www.treehugger.com/thmb/wfKpiCka8T8HsLKMN2kXNHm462U=/3798x3798/smart/filters:no_upscale()/pooja-sajan-wp3peZT5U-c-unsplash-5d3877dd1d5f496e87941edd690c25f3.jpg")
+# User.create(name: "Amy Gajkowski", email: "amy@gmail.com", password: "password", password_confirmation: "password", location: "Milwaukee, WI", twitter: "@kazames", about: "The books that help you the most are those which make you think the most.", image: "https://urbanmilwaukee.com/wp-content/uploads/2020/12/Amy3-6-768x768.jpg")
+# User.create(name: "Kristin Bissell", email: "kristin@gmail.com", password: "password", password_confirmation: "password", location: "Waukesha, WI", twitter: "@kristinbissell", about: "Be a flamingo in a flock of pigeons.", image: "https://scontent-ort2-1.xx.fbcdn.net/v/t1.18169-9/1928817_504070641821_9611_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=de6eea&_nc_ohc=sEvfRNeiSz8AX8TUZb1&tn=2RJkG2OtKrbB3WsI&_nc_ht=scontent-ort2-1.xx&oh=00_AT9dpbCLxQsp8uDp4YGm3akkoLYM1aXOAmPYT7v58TdTVw&oe=62420F02")
+
+# #Clubs
+
+# Club.create(name: "More Than Words", work_id: 550168, isbn: 9780399590504, is_active: true) #Educated by Tara Westover
+# Club.create(name: "Page Turners", work_id: 326569, isbn: 9780451524935, is_active: true) #1984 by George Orwell
+# Club.create(name: "Digital Get Down", work_id: 586568, isbn: 9780525560586, is_active: true) #Those Who Knew by Idra Novey
+# Club.create(name: "LadyBros", work_id: 653196, isbn: 9780593230251, is_active: false) #Caste: The Origins of Our Discontents, by Isabel Wilerson 
+# Club.create(name: "LadyBros 2.0", work_id: 50048, isbn: 9780375759642, is_active: true) #The Beautiful and the Damned, F Scott Fitzgerald
+
+# #Memberships
+
+# Membership.create(club_id: 1, user_id: 1)
+# Membership.create(club_id: 1, user_id: 4)
+# Membership.create(club_id: 1, user_id: 8)
+# Membership.create(club_id: 1, user_id: 3)
+# Membership.create(club_id: 2, user_id: 1)
+# Membership.create(club_id: 2, user_id: 4)
+# Membership.create(club_id: 2, user_id: 8)
+# Membership.create(club_id: 2, user_id: 2)
+# Membership.create(club_id: 3, user_id: 2)
+# Membership.create(club_id: 3, user_id: 3)
+# Membership.create(club_id: 3, user_id: 9)
+# Membership.create(club_id: 3, user_id: 10)
+# Membership.create(club_id: 4, user_id: 5)
+# Membership.create(club_id: 4, user_id: 6)
+# Membership.create(club_id: 4, user_id: 7)
+# Membership.create(club_id: 4, user_id: 9)
+# Membership.create(club_id: 5, user_id: 5)
+# Membership.create(club_id: 5, user_id: 6)
+# Membership.create(club_id: 5, user_id: 7)
+# Membership.create(club_id: 5, user_id: 10)
+
+# #Messages
+
+# Message.create(club_id: 1, user_id: 1, body: "Question 1: Many of Tara's father's choices have an obvious impact on Tara's life, but how did her mother's choices influence her? Tara's mom is complicit!!! HOW CAN YOU DO THAT TO A CHILD??!?!?!?!", category: "formal")
+# Message.create(club_id: 1, user_id: 4, body: "Question 2: Tara's brother Tyler tells her to take the ACT. What motivates Tara to follow his advice? I thought the seeds of her motivation were planted when the first thing happened, but really what forced her decision was the second thing.", category: "formal")
+# Message.create(club_id: 1, user_id: 8, body: "Question 2: Tara's brother Tyler tells her to take the ACT. What motivates Tara to follow his advice? Oh really? I totally thought it was the second thing, because that's when she...", category: "formal")
+# Message.create(club_id: 1, user_id: 1, body: "Wow, Tara's story is so inspring!!! OMG!", category: "informal")
+# Message.create(club_id: 1, user_id: 3, body: "Karen, you would think that. I found it incredibly scary a family like that could exist... and more probably do.", category: "informal")
+# Message.create(club_id: 2, user_id: 4, body: "Contradiction plays a huge role within the framework of Doublethink...", category: "formal")
+# Message.create(club_id: 2, user_id: 1, body: "I didn't think War is Peace was a contradiction. There is no contradiction on a grand scale...", category: "formal")
+# Message.create(club_id: 2, user_id: 8, body: "What would you do to undermine The Party? Knowing what you know now, how would you extricate yourself from the fate that awaits you? I would... ", category: "formal")
+# Message.create(club_id: 2, user_id: 2, body: "Can we talk about how much this is like today????", category: "informal")
+# Message.create(club_id: 2, user_id: 4, body: "Wow, and we barely survived the Trump years.", category: "informal")
+# Message.create(club_id: 3, user_id: 2, body: "Question 1: Idra Novey chose to set most of her novel in an unnamed place. Why? I loved this choice; I found myself flirting in and out of historical fact and questioning my stereocategorys of various islands.", category: "formal")
+# Message.create(club_id: 3, user_id: 3, body: "Question 1: Idra Novey chose to set most of her novel in an unnamed place. Why? UGH I HATED THIS. Like, just say it was Cuba. It was so obvious that's what she was trying to do...", category: "formal")
+# Message.create(club_id: 3, user_id: 9, body: "Did you guys also question what your silence is costing us?", category: "informal")
+# Message.create(club_id: 3, user_id: 10, body: "Question 2: Those Who Knew begins with Lena but quickly introduces several other characters points of view, creating a mosaic of narrative voices. How did that choice keep the plot moving? I loved how she wrote this. It was like each new character advanced the plot the tinest bit, and added new information that felt like the urgency of the mystery was ramping up. SPOILER ALERT: The ending is...", category: "formal")
+# Message.create(club_id: 3, user_id: 10, body: "Wow, I thought she really spoke with uncommon prescience to the swirl that is all around us", category: "informal")
+# Message.create(club_id: 4, user_id: 5, body: "I thought Wilkerson points us forward to ways America can move beyond the artificial and destructive separations of human divisions, toward hope in our common humanity.", category: "formal")
+# Message.create(club_id: 4, user_id: 6, body: "By linking the caste systems of America, India, and Nazi Germany, Wilkerson explores eight pillars that underlie caste systems across civilizations, including divine will, bloodlines, stigma, and more.", category: "formal")
+# Message.create(club_id: 4, user_id: 7, body: "After reading about the surprising health costs of caste, in depression and life expectancy, and the effects of this hierarchy on our culture and politics, I cannot help but wonder what a world we would live in without a caste.", category: "formal")
+# Message.create(club_id: 4, user_id: 9, body: "This was eye-opening. I loved how it blended people and history, and reexaminationed what lies under the surface of our ordinary lives and of American life today. In my life, I can think of...", category: "formal")
+# Message.create(club_id: 4, user_id: 5, body: "And we thought America wasn't racist because we elected Obama... eye roll.", category: "informal")
+# Message.create(club_id: 5, user_id: 5, body: "Question 1: In her introduction, Susan Orlean says that, like everything else Fitzgerald wrote, This Side of Paradise is a treatise about class. I agree..", category: "formal")
+# Message.create(club_id: 5, user_id: 6, body: "Question 3: Are the romantic pursuits of Amory and his friends primarily satisfying or disillusioning? How does money, or the lack of it, play a part in the pursuit of love? I thought they were disillusioning. There is no way without money any of these relationships survive...", category: "formal")
+# Message.create(club_id: 5, user_id: 7, body: "Question 7: Is This Side of Paradise in any way a tragic novel? How does it attempt to explain tragedy or loss? Do you think Fitzgerald intended a mournful or ultimately hopeful perspective? Why or why not? I'm laughing all the way to the bank as I write this. How can this not be a tragic novel? Isn't that why we read it...", category: "formal")
+# Message.create(club_id: 5, user_id: 8, body: "Oh Lila, you be cray.", category: "informal")
+# Message.create(club_id: 5, user_id: 6, body: "Can you even imagine if Leo played Amory??? Haaa ha ha ha ha.", category: "informal")
