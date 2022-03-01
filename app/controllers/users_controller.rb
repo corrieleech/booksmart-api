821af2cfa13cdbe26bf0b40c977d91b9032ea304
Corrie <corrieleech@gmail.com>
2022-03-01 10:57:21 -0600

    adds RESTful routes for users create, show, update and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user, only: [:show, :update, :destroy]
+  
   def create
     user = User.new(
       name: params[:name],
@@ -12,6 +13,40 @@ class UsersController < ApplicationController
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
+  end
+  
+  def show
+    user = User.find(params[:id])
+    if current_user == user 
+      render json: user
+    else
+      render json: {message: [], status: :bad_request}
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if current_user == user 
+      user.name = params[:name]|| current_user.name
+      user.email = params[:email] || current_user.email
+      user.image = params[:image] || current_user.image
+      user.location = params[:location] || current_user.location
+      user.about = params[:about] || current_user.about
+      user.twitter = params[:twitter] || current_user.twitter
+      if user.save
+        render json: {message: "#{user.name}'s profile has been updated.", profile: user, status: :ok}
+      else
+        render json: {errors: user.errors.full_messages, status: :unprocessable_entity }
+      end
+    else 
+      render json: {errors: user.errors.full_messages, status: :bad_request }
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: {message: "User has been removed from the database."}
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
+  get "/users/:id" => "users#show"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
+
+  get "/clubs" => "clubs#index"
+  post "/clubs" => "clubs#create"
+  get "/clubs/:id" => "clubs#show"
+  patch "/clubs/:id" => "clubs#update"
+
   post "/sessions" => "sessions#create"
-  
+ 
 end
