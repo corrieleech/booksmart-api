108b2a5b36ea496196a204518ce07e63a53b9531
Corrie <corrieleech@gmail.com>
2022-03-18 14:04:38 -0500

    adds low-level data caching for clubs model

--- app/models/club.rb
+++ app/models/club.rb
@@ -4,16 +4,18 @@ class Club < ApplicationRecord
   has_many :messages
   
   def book
+    Rails.cache.fetch([self, :book], expires_in: 1.day) do
     api_key = Rails.application.credentials.rh_api
     response = HTTP.get("https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/titles/#{isbn}?&api_key=#{api_key}")
     book_data = response.parse(:json)["data"]["titles"][0]
-    return {
+    {
       title: book_data["title"],
       subtitle: book_data["subtitle"],
       author: book_data["author"],
       pages: book_data["pages"],
       cover_image: book_data["_links"][1]
     }
+    end
   end
 
   def details
